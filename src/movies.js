ba0a3f6d70197eea2348c4b2a360ee1ffd653ea3
borimom <101846351+borimom@users.noreply.github.com>
2022-03-23 10:55:18 +0900

    Add files via upload

---
+++ src/movies.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import './movies.css';
+
+//yarn add prop-types
+
+ 
+function Movie({id, year, title, summary, poster}){
+   return(
+    <div className="movie">
+        <img src={poster} alt={title}/>
+        <h5 className="movie__title">{title}</h5>
+        <h5 className="movie__year">{year}</h5>
+        <h5 className="movie__summary">{summary}</h5>
+    </div>
+   )
+
+}
+
+Movie.propTypes ={ //타입검사
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.number.isRequired,
+    summary: PropTypes.number.isRequired,
+    poster: PropTypes.number.isRequired,
+
+}
+
+
+export default Movie;
